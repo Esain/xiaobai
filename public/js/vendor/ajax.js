f5a0549fc3105aa6386a7804f75e7ca6f71fe98f
杨亦昕 <yangyx@kingnet.com>
2018-01-03 17:53:23 +0800

    新增功能js

---
+++ public/js/vendor/ajax.js
@@ -0,0 +1,76 @@
+define(['p','common'], function (p, util) {
+	var postServer = '/';
+	return {
+		ajaxPost: function(url, param){
+			var ajaxOpt = {
+				type: 'POST',
+				url: url,
+				data: JSON.string(param)
+			}
+			return this.ajax(ajaxOpt)
+		},
+		ajaxGet: function(url, param){
+			var ajaxOpt = {
+				type: 'GET',
+				url: url,
+				data: param
+			}
+			return this.ajax(ajaxOpt);
+		},
+		ajax: function(ajaxOpt){
+			var myPromise = Promise || p;
+		
+			return new myPromise(function(resolve, reject){
+				var postUrl = ajaxOpt.url;
+				if(ajaxOpt.url.indexOf('http') == -1){
+					postUrl = postServer + url;
+				}
+				var defaultOpt = {
+					 	url: postUrl,
+					  	success: success,
+					  	error: error,
+					  	complete: complete,
+					  	beforeSend: beforeSend,
+					  	contentType:'application/json',
+					  	// headers: {
+					  	// 	'token': token
+					  	// },
+					  	timeout: 8000
+					}
+				ajaxOpt = $({}, defaultOpt, ajaxOpt);
+
+				$.ajax(ajaxOpt);
+				
+				function success(data){
+					
+					if(data.code == 0|| data.code == null){
+						resolve(data.data);
+					}else if(data.code == -1){
+						reject(data.message);
+						t.loginErrPage();
+					}else{
+						util.warningTip({
+							title: '错误信息',
+							context:data.message,
+						});
+						reject(data.message);
+					}
+				}
+				function error(XMLHttpRequest, textStatus){
+					util.warningTip({
+						title: '错误信息',
+						context: '网络异常,请稍后再试',
+					});
+					
+					reject(JSON.stringify(XMLHttpRequest));
+				}
+				function complete(){
+					// $('.inline-loading').remove();
+				}
+				function beforeSend(){
+					// $('body').loading();
+				}
+			}
+		}
+	}
+});
---
+++ public/js/vendor/common.js
@@ -0,0 +1,70 @@
+define(function() {
+    return util = {};
+
+    util.getCountDown = function(fn) {
+        var CountDown = {};
+        CountDown.count = 60;
+        CountDown.start = function() {
+            var self = CountDown;
+            self._a = setTimeout(function() {
+                if (self.count === 0) {
+                    self.count = 60
+                    fn('获取验证码', false);
+                } else {
+                    self.start()
+                    self.count--
+                        fn(self.count + '秒', true);
+                }
+            }, 1000)
+        }
+
+        CountDown.reset = function() {
+            CountDown.count = 60;
+            clearTimeout(CountDown._a);
+            fn('获取验证码', false)
+        }
+
+        return CountDown;
+    }
+
+    var _index = 0;
+    util.warningTip = function(opt) {
+        var defaultOpt = {
+            title: '提示信息',
+            context: '',
+        }
+
+        opt = $.extend({}, defaultOpt, opt)
+        _index++;
+
+        var $tpl = $('<div class="weui-tips">\
+				        <div class="weui-mask"></div>\
+				        <div class="weui-dialog">\
+				            <div class="weui-dialog__hd"><strong class="weui-dialog__title">' +
+            title +
+            '</strong></div>\
+				            <div class="weui-dialog__bd">' +
+            context +
+            '</div>\
+				            <div class="weui-dialog__ft">\
+				                <a class="weui-dialog__btn weui-dialog__btn_primary">确定</a>\
+				            </div>\
+				        </div>\
+				    </div>');
+        $tpl.data('index', _index);
+
+        $tpl.find('.dialog__btn').on('click', function(e) {
+            opt.cb && opt.cb(e);
+            $tpl.remove();
+            $tpl = null;
+        });
+    }
+
+    util.warningTip({ title: '托尔斯泰', context: '时时时时时时s', cb: function() { cosole.log(1) } })
+
+    function tipsOff(e) {
+        var self = $(e.target).closest('.weui-tips').addClass('hide')
+    }
+
+
+})
