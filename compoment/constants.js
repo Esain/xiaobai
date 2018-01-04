15dd9e7445073113bc40eb04c7a6e10039f3ba4b
杨亦昕 <yangyx@kingnet.com>
2018-01-04 16:55:04 +0800

    微信授权

--- compoment/constants.js
+++ compoment/constants.js
@@ -3,23 +3,23 @@ var constants = function () {
 
 	this.POST_HOST = "https://api.weixin.qq.com/"
 
-	this.ACCESS_URL = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + env.WEI_XIN_APP_ID + "&secret=" + env.WEI_XIN_SECRET;
-	this.TICKET_URL = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi";
-	this.MENU_URL = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=";
-	this.SEND_ASYN_URL = "https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=";
-	this.UPLOAD_MEDIA = "https://api.weixin.qq.com/cgi-bin/media/upload?access_token=";
+	this.ACCESS_URL = `${this.POST_HOST}cgi-bin/token?grant_type=client_credential&appid=${env.WEI_XIN_APP_ID}&secret=${env.WEI_XIN_SECRET}`;
+	this.TICKET_URL = `${this.POST_HOST}cgi-bin/ticket/getticket?type=jsapi`;
+	this.MENU_URL = `${this.POST_HOST}cgi-bin/menu/create?access_token=`;
+	this.SEND_ASYN_URL = `${this.POST_HOST}cgi-bin/message/custom/send?access_token=`;
+	this.UPLOAD_MEDIA = `${this.POST_HOST}cgi-bin/media/upload?access_token=`;
 
-	this.STATISTICS_GETLIST = "https://api.weixin.qq.com/customservice/msgrecord/getrecord?access_token=";
-	this.CUST_SERV_GETLIST = "https://api.weixin.qq.com/cgi-bin/customservice/getkflist?access_token=";
-	this.CUST_SERV_GETONLINELIST = "https://api.weixin.qq.com/cgi-bin/customservice/getonlinekflist?access_token=";
+	this.STATISTICS_GETLIST = `${this.POST_HOST}customservice/msgrecord/getrecord?access_token=`;
+	this.CUST_SERV_GETLIST = `${this.POST_HOST}cgi-bin/customservice/getkflist?access_token=`;
+	this.CUST_SERV_GETONLINELIST = `${this.POST_HOST}cgi-bin/customservice/getonlinekflist?access_token=`;
 
-	this.GET_MEDIA = "https://api.weixin.qq.com/cgi-bin/media/get?access_token=";
+	this.GET_MEDIA = `${this.POST_HOST}cgi-bin/media/get?access_token=`;
 
 	this.PAGE_SIZE = 50;
 
-	this.AUTH_URL = `${this.POST_HOST}connect/oauth2/authorize?appid=${env.WEI_XIN_APP_ID}&redirect_uri=${encodeURIComponent(env.REDIRECT_URI)}&response_type=code&scope=snsapi_base&state=test11#wechat_redirect`;
+	this.AUTH_URL = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${env.WEI_XIN_APP_ID}&redirect_uri=${encodeURIComponent(env.REDIRECT_URI)}&response_type=code&scope=snsapi_base&state=test11#wechat_redirect`;
 	this.getAuthAccessUrl = function (code) {
-		return `${this.POST_HOST}ns/oauth2/access_token?appid=APPID&secret=SECRET&code=${code}&grant_type=authorization_code`;
+		return `${this.POST_HOST}sns/oauth2/access_token?appid=APPID&secret=SECRET&code=${code}&grant_type=authorization_code`;
 	}
 }
 var obj = new constants();
