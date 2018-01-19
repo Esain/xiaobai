b2221c75ec5f7afcc040b0539547f9b3c370b26c
杨亦昕 <yangyx@kingnet.com>
2018-01-19 18:07:36 +0800

    login测试

--- endpoint/login.js
+++ endpoint/login.js
@@ -1,30 +1,38 @@
 var path = require('path')
 var request = require('request');
 var constants = require('../compoment/constants');
-
+var codeMap = {}
 module.exports = function (req, resp, param, next) {
     if (req.cookies && req.cookies.openID){
+        console.log('cookie opendID: ',req.cookies.openID)
         resp.redirect("/");
     }else if(!param.code && !param.state){
         resp.redirect(constants.AUTH_URL);
     }else{
         var code = param.code;
-        request
-            .get(constants.getAuthAccessUrl(code), function (error, response, body) {
-                if(error){
+        if(codeMap[code]){
+            console.log('codeMap opendID: ', codeMap[code])
+            resp.cookie('openID', codeMap[code], { maxAge: Date.now() + 72000000 });
+            resp.redirect('/');
+        }else{
+            request
+                .get(constants.getAuthAccessUrl(code), function (error, response, body) {
+                    if(error){
 
-                }else{
-                    var reqObj = JSON.parse(body)
-                    if (reqObj.errcode) {
-                        console.error(`获取token失败${reqObj.errcode} ---- ${reqObj.errmsg}`);
-                        var error = new Error('获取token失败')
-                        next(error);
-                    } else {
-                        resp.cookie('openID', reqObj.openid, { maxAge: Date.now() + 72000000 });
-                        resp.redirect('/');
+                    }else{
+                        var reqObj = JSON.parse(body)
+                        if (reqObj.errcode) {
+                            console.error(`获取token失败${reqObj.errcode} ---- ${reqObj.errmsg}`);
+                            var error = new Error('获取token失败')
+                            next(error);
+                        } else {
+                            console.log('remote opendID: ', codeMap[code])
+                            resp.cookie('openID', reqObj.openid, { maxAge: Date.now() + 72000000 });
+                            resp.redirect('/');
+                        }
                     }
-                }
-            })
+                })
+        }
           
     }
 }
