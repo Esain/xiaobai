13daaec0b369b634c38deca9921418e0c61fefc5
杨亦昕 <yangyx@kingnet.com>
2018-01-31 18:18:33 +0800

    任务改成批量修改

--- public/js/task.js
+++ public/js/task.js
@@ -1,16 +1,7 @@
 require(['zepto', 'weui', 'ajax', 'md5', 'common'], function ($, weui, ajax, md5, util) {
     var accountNumber;
-    var actions = [{
-        label: '起床',
-        value: 0
-    }, {
-        label: '睡觉',
-        value: 1
-    }, {
-        label: '讲故事',
-        value: 2
-    }]
-
+    var actions = [];
+    var changeActions = [];
     function renderPgaes() {
         actions.forEach(function (item) {
             var tpl = getTpl(item)
@@ -18,10 +9,10 @@ require(['zepto', 'weui', 'ajax', 'md5', 'common'], function ($, weui, ajax, md5
         });
 
         $('.main-panel').on('click', '.weui-cell', function (e) {
-            console.log($(this));
-            var oldaction = $(this).find(".weui-cell__bd").text();
-            var oldtime = $(this).find(".weui-cell__ft span").text();
-            oldtime = oldtime.toString().split("提醒")[0];
+            // console.log($(this));
+            var data = $(this).data('o-data');
+            var oldaction = data.label;
+            var oldtime = data.time;
             setTime(oldaction, oldtime, e);
         });
     }
@@ -31,12 +22,14 @@ require(['zepto', 'weui', 'ajax', 'md5', 'common'], function ($, weui, ajax, md5
         if (item.time && item.time != null) {
             timestr = item.time + "提醒";
         }
-        return '<div class="weui-cell weui-cell_access" data-type="' + item.value + '">\n' +
+        var $tpl = $('<div class="weui-cell weui-cell_access" data-type="' + item.value + '">\n' +
             '            <div class="weui-cell__bd">' + item.label + '</div>\n' +
             '            <div class="weui-cell__ft">\n' +
             '                <span class="val mr5">' + timestr + '</span>\n' +
             '            </div>\n' +
-            '        </div>'
+            '        </div>')
+            $tpl.data('o-data', JSON.stringify(item));
+        return $tpl;
     }
 
 
@@ -52,7 +45,12 @@ require(['zepto', 'weui', 'ajax', 'md5', 'common'], function ($, weui, ajax, md5
                 var newaction = $this.find(".weui-cell__bd").text();
                 var newtime = $this.find(".weui-cell__ft span").text();
                 newtime = (newtime + "").split("提醒")[0];
-                modifyTask(oldaction, oldtime, newaction, newtime);
+                // modifyTask(oldaction, oldtime, newaction, newtime);
+                var t = $this.data('o-data');
+                t.label = newaction;
+                t.time = newtime;
+                $this.data('o-data', JSON.stringify(t));
+                // updateChangeActions(oldaction, oldtime, newaction, newtime)
             }
         });
     }
@@ -109,24 +107,58 @@ require(['zepto', 'weui', 'ajax', 'md5', 'common'], function ($, weui, ajax, md5
         }).catch(function (error) { });
     }
 
-    function modifyTask(oldaction, oldtime, newaction, newtime) {
-        var valuestr = JSON.stringify({
-            accountNumber: accountNumber,
-            action: oldaction,
-            time: oldtime,
-            newAction: newaction,
-            newTime: newtime
+    // function modifyTask(oldaction, oldtime, newaction, newtime) {
+    //     var valuestr = JSON.stringify({
+    //         accountNumber: accountNumber,
+    //         action: oldaction,
+    //         time: oldtime,
+    //         newAction: newaction,
+    //         newTime: newtime
+    //     });
+    //     ajax.ajaxPost('baymin/updatehabit', {
+    //         key: md5("8d98b93a0d4e1777acb36d4404c61854" + valuestr),
+    //         value: valuestr
+    //     }).then(function (res) {
+    //         switch (res.status) {
+    //             case 0: //成功
+    //                 localStorage.setItem("relation", brela);
+    //                 localStorage.setItem("cname", bname);
+    //                 localStorage.setItem("birthday", bage);
+    //                 localStorage.setItem("sex", bsex);
+    //                 break;
+    //             default:
+    //                 util.warningTip({
+    //                     title: '设置宝宝信息失败',
+    //                     context: res.msg,
+    //                     cb: function () {
+    //                     }
+    //                 })
+    //                 break;
+    //         };
+    //     }).catch(function (error) { });
+    // }
+
+    var submit = function(){
+        var postModel = { accountNumber: accountNumber };
+        changeActions.length = 0;
+        $('.weui-cell').each(function(i, dom) {
+            var data =  $(dom).data('o-data');
+            changeActions.push({
+                action: data.label,
+                tiem: data.time
+            })
         });
-        ajax.ajaxPost('baymin/updatehabit', {
+        postModel.habits = changeActions;
+        // console.log(postModel)
+
+        var valuestr = JSON.stringify(postModel);
+        return ajax.ajaxPost('baymin/updatehabit', {
             key: md5("8d98b93a0d4e1777acb36d4404c61854" + valuestr),
             value: valuestr
         }).then(function (res) {
             switch (res.status) {
                 case 0: //成功
-                    localStorage.setItem("relation", brela);
-                    localStorage.setItem("cname", bname);
-                    localStorage.setItem("birthday", bage);
-                    localStorage.setItem("sex", bsex);
+                    return 1;
                     break;
                 default:
                     util.warningTip({
@@ -135,24 +167,27 @@ require(['zepto', 'weui', 'ajax', 'md5', 'common'], function ($, weui, ajax, md5
                         cb: function () {
                         }
                     })
+                    throw new Error('设置失败');
                     break;
             };
-        }).catch(function (error) { });
+        })
     }
 
     getTask();
 
     $(".task .check").click(function () {
-        if (sessionStorage.getItem('isBindedEnd=="true"') && sessionStorage.getItem('isBinded=="true"')) {
-            location.hash = "account";
-        }else{
-            sessionStorage.setItem("isBindedEnd", "true");
-            util.warningTip({
-                title: '设置成功',
-                context: "用户设置已完成！",
-                cb: function () {
-                }
-            })
-        }
+        submit().then(function (params) {
+            if (sessionStorage.getItem('isBindedEnd=="true"') && sessionStorage.getItem('isBinded=="true"')) {
+                location.hash = "account";
+            }else{
+                sessionStorage.setItem("isBindedEnd", "true");
+                util.warningTip({
+                    title: '设置成功',
+                    context: "用户设置已完成！",
+                    cb: function () {
+                    }
+                })
+            }
+        }).catch(function (error) { console.log(error)});;
     });
 })
